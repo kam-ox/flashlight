2024fcec88ab298e57059cd72d3da42851535b42
kamox <kamilcc@mail.ru>
2021-06-04 10:09:47 +0600

    build UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animator/animator.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_flashlight/flutter_flashlight.dart';
@@ -14,20 +15,56 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: Center(
-          child: Column(
-            children: <Widget>[
-              ElevatedButton(
-                  child: Text('Turn on'),
-                  onPressed: () {
-                    Flashlight.lightOn();
-                  }),
-              ElevatedButton(
-                  child: Text('Turn off'),
-                  onPressed: () {
-                    Flashlight.lightOff();
-                  })
-            ],
+          child: Container(
+            height: 200,
+            width: 200,
+            child: Animator<double>(
+              duration: Duration(milliseconds: 1500),
+              curve: Curves.easeInOut,
+              tween: Tween<double>(begin: 0, end: 15),
+              cycles: 0,
+              builder: (context, animatorState, child) => Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.red[400],
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.red[600].withOpacity(0.5),
+                      offset: Offset(0, 5),
+                      blurRadius: 40,
+                    ),
+                  ],
+                ),
+                margin: EdgeInsets.all(animatorState.value),
+                child: Center(
+                  child: Container(
+                    child: Wrap(
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              'Tap to Turn',
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              'ON',
+                              style: TextStyle(fontSize: 35),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ),
         ),
       ),
