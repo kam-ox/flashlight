fa22e8cd23b7d04e2a7ad6302bb46a9f345d69ed
kamox <kamilcc@mail.ru>
2021-06-04 11:33:46 +0600

    finished app v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:animator/animator.dart';
 import 'package:flutter/material.dart';
 
+import 'package:animator/animator.dart';
 import 'package:flutter_flashlight/flutter_flashlight.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,11 +12,63 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  bool isOn = false;
+
+  // bool _hasFlashlight = false;
+  // initFlashlight() async {
+  //   bool hasFlash = await Flashlight.hasFlashlight;
+  //   print("Device has flash ? $hasFlash");
+  //   setState(() {
+  //     _hasFlashlight = hasFlash;
+  //   });
+  // }
+
+  void controlTorch() {
+    if (isOn) {
+      Flashlight.lightOn();
+      setState(() {
+        isOn = !isOn;
+      });
+      Fluttertoast.showToast(
+          msg: "You Turned Flashlight ON",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.TOP_LEFT,
+          timeInSecForIosWeb: 1,
+          textColor: Colors.black,
+          fontSize: 14.0);
+    } else if (!isOn) {
+      Flashlight.lightOff();
+      setState(() {
+        isOn = !isOn;
+      });
+      Fluttertoast.showToast(
+          msg: "You Turned Flashlight OFF",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.TOP_LEFT,
+          timeInSecForIosWeb: 1,
+          textColor: Colors.black,
+          fontSize: 14.0);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.black,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            'Flashlight',
+            style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Teko',
+            ),
+          ),
+          backgroundColor: Colors.black,
+        ),
         body: Center(
           child: Container(
             height: 200,
@@ -25,41 +78,47 @@ class _MyAppState extends State<MyApp> {
               curve: Curves.easeInOut,
               tween: Tween<double>(begin: 0, end: 15),
               cycles: 0,
-              builder: (context, animatorState, child) => Container(
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.red[400],
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.red[600].withOpacity(0.5),
-                      offset: Offset(0, 5),
-                      blurRadius: 40,
-                    ),
-                  ],
-                ),
-                margin: EdgeInsets.all(animatorState.value),
-                child: Center(
-                  child: Container(
-                    child: Wrap(
-                      children: [
-                        Column(
-                          children: [
-                            Text(
-                              'Tap to Turn',
-                              style: TextStyle(
-                                fontSize: 16,
+              builder: (context, animatorState, child) => GestureDetector(
+                onTap: () => controlTorch(),
+                child: Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: isOn ? Colors.green[400] : Colors.red[400],
+                    boxShadow: [
+                      BoxShadow(
+                        color: isOn
+                            ? Colors.green[600].withOpacity(0.5)
+                            : Colors.red[600].withOpacity(0.5),
+                        offset: Offset(0, 5),
+                        blurRadius: 40,
+                      ),
+                    ],
+                  ),
+                  margin: EdgeInsets.all(animatorState.value),
+                  child: Center(
+                    child: Container(
+                      child: Wrap(
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                'Tap To Turn',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                ),
                               ),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              'ON',
-                              style: TextStyle(fontSize: 35),
-                            ),
-                          ],
-                        ),
-                      ],
+                              Text(
+                                isOn ? 'ON' : 'OFF',
+                                style: TextStyle(
+                                  fontSize: 35,
+                                  color: isOn ? Colors.white : Colors.black,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
